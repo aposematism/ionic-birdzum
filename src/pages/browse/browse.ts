63cdd7e0c3e0933c2573c143d32ffd2dc4d609c3
Jacob Woods <mckiewjaco@ecs.vuw.ac.nz>
2018-08-20 17:21:21 +1200

    added dummy database function

--- src/pages/browse/browse.ts
+++ src/pages/browse/browse.ts
@@ -7,11 +7,56 @@ import { ClassifyPage } from '../classify/classify';
   templateUrl: 'browse.html'
 })
 export class BrowsePage {
-	
-	nativity:string = "any";
-	activity:string = "any";
-	habitat:string = "any";
+  
+  nativity:string = "any";
+  activity:string = "any";
+  habitat:string = "any";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  
+  search(){
+	console.log("Searching...");
+    // Get the birds (just use a dummy function for now)
+	let birds = this.getBirds(this.nativity, this.activity, this.habitat);
+	console.log(birds);
+	
+  }
+  
+  getBirds(n, a, h){
+	let allBirds = [
+	
+		{
+			name:"Kiwi",
+			activity:"nocturnal",
+			habitat:"land",
+			nativity:"native",
+			description:"Small and brown with a long beak."
+		},
+		{
+			name:"Kereru",
+			activity:"diurnal",
+			habitat:"land",
+			nativity:"native",
+			description:"Big, fat, likes berries."
+		},
+		{
+			name:"Seagull",
+			activity:"diurnal",
+			habitat:"sea",
+			nativity:"foreign",
+			description:"Steals chips."
+		},
+		{
+			name:"Pigeon",
+			activity:"diurnal",
+			habitat:"land",
+			nativity:"foreign",
+			description:"Sky rat."
+		}
+	
+	];
+	
+	return allBirds;
   }
 }
